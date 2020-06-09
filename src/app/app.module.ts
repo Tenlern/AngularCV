dca44439dc0c8e800c0887e2e3eaa986ec4c87a6
Tenlern <illyash20009@yandex.ru>
2020-06-10 00:33:53 +0300

    Add main app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CvComponent } from './cv/cv.component';
+import { ContentComponent } from './content/content.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    ContentComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
