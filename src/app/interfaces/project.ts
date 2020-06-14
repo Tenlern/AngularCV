325ede7b6d9d23e71ec2a6c7bab2fec0fd0564bf
Tenlern <illyash20009@yandex.ru>
2020-06-15 02:47:15 +0300

    Restyling

--- src/app/interfaces/project.ts
+++ src/app/interfaces/project.ts
@@ -1,6 +1,7 @@
 export interface Project {
   id: number;
   name: string;
+  description: string;
   icon_url: string;
   img_url: string;
 }
--- src/app/projects/projects.service.ts
+++ src/app/projects/projects.service.ts
@@ -6,9 +6,27 @@ import {Project} from '../interfaces/project';
 })
 export class ProjectsService {
   items: Project[] =  [
-    { id: 1, name: 'Test 1', icon_url: '/assets/img/projects/icon_1.jpg', img_url: '/assets/img/projects/1.jpeg' },
-    { id: 2, name: 'Test 2', icon_url: '/assets/img/projects/icon_2.jpg', img_url: '/assets/img/projects/2.jpeg' },
-    { id: 3, name: 'Test 3', icon_url: '/assets/img/projects/icon_3.jpg', img_url: '/assets/img/projects/3.jpeg' },
+    {
+      id: 1,
+      name: 'Test 1',
+      description: 'Жоп-арт, отражающий боль от архитектуры и отрицающий влияние бога на мир человека',
+      icon_url: '/assets/img/projects/icon_1.jpg',
+      img_url: '/assets/img/projects/1.jpeg'
+    },
+    {
+      id: 2,
+      name: 'Test 2',
+      description: 'Шрифт боли',
+      icon_url: '/assets/img/projects/icon_2.jpg',
+      img_url: '/assets/img/projects/2.jpeg'
+    },
+    {
+      id: 3,
+      name: 'Test 3',
+      description: 'Боль, просто боль. Не знаю, что вы тут хотите увидеть',
+      icon_url: '/assets/img/projects/icon_3.jpg',
+      img_url: '/assets/img/projects/3.jpeg'
+    },
   ];
 
 
